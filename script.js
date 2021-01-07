48059f6819afe73b977ea82405a01085578f4d80
monskvll <devmonskvll@gmail.com>
2021-01-07 00:11:14 +0000

    CSS fix, JS press function.

--- script.js
+++ script.js
@@ -17,3 +17,55 @@ function divide(x, y) {
 function operate() {
     
 }
+
+function displayPress()  {
+    document.getElementById("one").addEventListener("click", function() {
+        document.getElementById("display").textContent += "1";
+    });
+
+    document.getElementById("two").addEventListener("click", function() {
+        document.getElementById("display").textContent += "2";
+    });
+
+    document.getElementById("three").addEventListener("click", function() {
+        document.getElementById("display").textContent += "3";
+    });
+
+    document.getElementById("four").addEventListener("click", function() {
+        document.getElementById("display").textContent += "4";
+    });
+
+    document.getElementById("five").addEventListener("click", function() {
+        document.getElementById("display").textContent += "5";
+    });
+
+    document.getElementById("six").addEventListener("click", function() {
+        document.getElementById("display").textContent += "6";
+    });
+
+    document.getElementById("seven").addEventListener("click", function() {
+        document.getElementById("display").textContent += "7";
+    });
+
+    document.getElementById("eight").addEventListener("click", function() {
+        document.getElementById("display").textContent += "8";
+    });
+
+    document.getElementById("nine").addEventListener("click", function() {
+        document.getElementById("display").textContent += "9";
+    });
+
+    document.getElementById("zero").addEventListener("click", function() {
+        document.getElementById("display").textContent += "0";
+    });
+
+    document.getElementById("dot").addEventListener("click", function() {
+        document.getElementById("display").textContent += ".";
+    });
+
+    document.getElementById("clear").addEventListener("click", function() {
+        document.getElementById("display").textContent = "";
+    });
+}
+
+displayPress();
